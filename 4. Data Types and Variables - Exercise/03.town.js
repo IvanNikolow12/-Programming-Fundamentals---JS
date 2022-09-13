d3428648ce8f46c3de7df5f35762c27e9d593b24
Ivan <112165492+IvanNikolow12@users.noreply.github.com>
2022-09-13 21:17:12 +0300

    Add files via upload

---
+++ 4. Data Types and Variables - Exercise/03.town.js
@@ -0,0 +1,9 @@
+function townInfo (town, p, a){
+    let city = town;
+    let population = p;
+    let area = a;
+    console.log(`Town ${city} has population of ${population} and area ${area} square km.`)
+}
+townInfo ('Sofia',
+1286383,
+492)
