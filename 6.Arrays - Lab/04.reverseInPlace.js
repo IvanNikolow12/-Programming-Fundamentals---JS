993f2eda99e04f13a44b983df141e1ccdfc5c40e
Ivan <112165492+IvanNikolow12@users.noreply.github.com>
2022-09-23 08:34:38 +0300

    Add files via upload

---
+++ 6.Arrays - Lab/04.reverseInPlace.js
@@ -0,0 +1,7 @@
+function reverseInPlace(arr){
+    let myArr = arr.reverse();
+    console.log(myArr.join(" "))
+}
+reverseInPlace(['a', 'b', 'c', 'd', 'e'])
+reverseInPlace (['abc', 'def', 'hig', 'klm', 'nop'])
+reverseInPlace (['33', '123', '0', 'dd'])
